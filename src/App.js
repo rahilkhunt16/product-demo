c139557d61e05bede749ce33ff1431db0f910b79
rahilkhunt16 <rahilkhunt2006@gmail.com>
2024-05-17 08:04:05 +0530

    Demo-Done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import { BrowserRouter, Routes } from 'react-router-dom';
+import './App.css';
+
+
+<BrowserRouter>
+<Routes>
+        <router parth="/" element={'./login.js'} />
+       
+</Routes>
+
+</BrowserRouter>
+export default App;
---
+++ src/componet/Login.js
@@ -0,0 +1,18 @@
+import { Formik } from "formik";
+import { Form } from "react-router-dom";
+
+
+<form>
+    <input>
+        type="text"
+        value="text"
+        placeholder="enter user name"
+    
+    </input>
+    <input>
+        type="password"
+        value="password"
+        placeholder="enter user password"
+
+    </input>
+</form>
