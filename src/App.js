abab44198760fe8f929a252a2ef83fea7c4e0c15
rahilkhunt16 <rahilkhunt2006@gmail.com>
2024-05-17 09:03:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-
-import { BrowserRouter, Routes } from 'react-router-dom';
+import React from 'react';
 import './App.css';
+import Products from './features/products/products';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ProductPage } from './components/ProductPage';
+import LoginPage from './components/LoginPage';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Products />} />
+        <Route path='/product/:id' element={<ProductPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-<BrowserRouter>
-<Routes>
-        <router parth="/" element={'./login.js'} />
-       
-</Routes>
-
-</BrowserRouter>
 export default App;
