091a8cf304fd66242cc908504effb298976b28c9
rahilkhunt16 <rahilkhunt2006@gmail.com>
2024-05-20 08:50:08 +0530

    file updet

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useFormik } from "formik"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { UserLogin } from "../features/products/productSlice"
+import "./login.css"
 
 const LoginPage = () => {
     let navigate = useNavigate()
@@ -40,22 +41,40 @@ const LoginPage = () => {
     return (
         <>
             <form onSubmit={formik.handleSubmit}>
-                <div>
-                    <input type="text" onChange={formik.handleChange} placeholder="Enter Username...."
-                        name="username" value={formik.values.username}></input>
-                    {formik.errors.username && <p>{formik.errors.username}</p>}
-                </div>
-                <div>
-                    <input type="password" onChange={formik.handleChange} placeholder="Enter Password...."
-                        name="password" value={formik.values.password}></input>
-                    {formik.errors.password && <p>{formik.errors.password}</p>}
-                </div>
-                <div>
-                    <button type="submit" style={{ padding: '3px 10px' }}>LOGIN</button>
-                </div>
+
+                <section class="container">
+                    <div class="login-container">
+                        <div class="circle circle-one"></div>
+                        <div class="form-container">
+                            <img src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png" alt="illustration" class="illustration" />
+                            <h1 class="opacity">LOGIN</h1>
+                            <form>
+                                <input type="text" onChange={formik.handleChange} placeholder="Enter Username...."
+                                    name="username" value={formik.values.username}></input>
+                                {formik.errors.username && <p>{formik.errors.username}</p>}
+                                <input type="password" onChange={formik.handleChange} placeholder="Enter Password...."
+                                    name="password" value={formik.values.password}></input>
+                                {formik.errors.password && <p>{formik.errors.password}</p>}
+                            </form>
+                            <div>
+                                <button type="submit" style={{ padding: '3px 10px' }}>LOGIN</button>
+                            </div>
+                            <div class="register-forget opacity">
+                                <a href="">REGISTER</a>
+                                <a href="">FORGOT PASSWORD</a>
+                            </div>
+                        </div>
+                        <div class="circle circle-two"></div>
+                    </div>
+                    <div class="theme-btn-container"></div>
+                </section>
+
             </form>
         </>
     )
 }
 
 export default LoginPage
+
+
+   
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -4,7 +4,6 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getData } from "../features/products/productSlice"
 
-
 export const ProductPage = () => {
     let dispatch = useDispatch()
     const singleProductData = useSelector((state) => state.product.singleProductData)
@@ -39,21 +38,6 @@ export const ProductPage = () => {
                             }
                         </div>
                     </div>
-                    {/* <div className="img-item">
-                                <a href="#" data-id="2">
-                                    <img src="https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_2.jpg" alt="shoe image" />
-                                </a>
-                            </div>
-                            <div className="img-item">
-                                <a href="#" data-id="3">
-                                    <img src="https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_3.jpg" alt="shoe image" />
-                                </a>
-                            </div>
-                            <div className="img-item">
-                                <a href="#" data-id="4">
-                                    <img src="https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_4.jpg" alt="shoe image" />
-                                </a>
-                            </div> */}
                     <div className="product-content">
                         <h2 className="product-title">{singleProductData?.title}</h2>
                         <a href="#" className="product-link">visit {singleProductData?.brand} store</a>
@@ -65,14 +49,12 @@ export const ProductPage = () => {
                             <i className="fas fa-star-half-alt"></i>
                             <span>{singleProductData?.rating?.toFixed(1)}({singleProductData?.stock})</span>
                         </div>
-
                         <div className="product-price">
                             <p className="last-price">Old Price: <span>${singleProductData?.price}</span></p>
                             <p className="new-price">New Price: <span>${(singleProductData?.price) - (singleProductData?.discountPercentage * singleProductData?.price / 100).toFixed(2)} ({singleProductData?.discountPercentage})</span></p>
                         </div>
-
                         <div className="product-detail">
-                            <h2>about this item: </h2>
+                            <h2 className="about">about this item: </h2>
                             <p>{singleProductData?.description}</p>
                             <ul>
                                 <li>Available: <span>{singleProductData?.stock > 0 ? `in stock` : `out of stock`}</span></li>
@@ -81,7 +63,6 @@ export const ProductPage = () => {
                                 <li>Shipping Fee: <span>Free</span></li>
                             </ul>
                         </div>
-
                         <div className="purchase-info">
                             <input type="number" min="0" value="1" />
                             <button type="button" className="btn">
